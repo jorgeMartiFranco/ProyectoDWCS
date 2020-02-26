cf63bd9cf5e3ce9202768ff73ee6042a9c4dc620
Diego Rodríguez Vicente <diego_pkv@hotmail.com>
2020-02-26 19:41:54 +0100

    Corregida la consulta de login para que sea case-insensitive

--- www/db.php
+++ www/db.php
@@ -2,8 +2,9 @@
 function checkUser($username, $password) {
     $params = parse_ini_file("db.ini");
     $pdo = new PDO("mysql:host=$params[host];dbname=$params[dbname];charset=$params[charset]","$params[username]");
-    $stmt = $pdo->prepare('SELECT usuario, email, `password` FROM socios WHERE usuario=:username OR email=:username');
-    $stmt->execute([':username' => $username]);
+    $stmt = $pdo->prepare('SELECT usuario, email, `password` FROM socios WHERE lower(usuario)=:username OR lower(email)=:username');
+    $stmt->execute([':username' => strtolower($username)]);
+    
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     
