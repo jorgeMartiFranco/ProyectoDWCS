786af6aec685f4819dd22e5c3525b7389061f0bf
Diego Rodríguez Vicente <diego_pkv@hotmail.com>
2020-03-07 22:29:59 +0100

    Añadido soporte PHP para el formulario de registro y comprobaciones para las conexiones de PDO

--- www/db.php
+++ www/db.php
@@ -1,70 +1,112 @@
 <?php
 
 function load($user) {
-
     $params = parse_ini_file("db-$user.ini");
     $pdo = new PDO("mysql:host=$params[host];dbname=$params[dbname];charset=$params[charset]", "$params[username]");
     return $pdo;
 }
 
 function checkUser($username, $password) {
-    $pdo = load("root");
-    $stmt = $pdo->prepare('SELECT usuario, email, `password` FROM socios WHERE lower(usuario)=:username OR lower(email)=:username');
-    $stmt->execute([':username' => strtolower($username)]);
-
-
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($user !== FALSE) {
-        if (password_verify($password, $user['password'])) {
-            return $user;
+    if($pdo = load("root")) {
+        $stmt = $pdo->prepare('SELECT usuario, email, `password` FROM socios WHERE lower(usuario)=:username OR lower(email)=:username');
+        $stmt->execute([':username' => strtolower($username)]);
+    
+    
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        if ($user !== FALSE) {
+            if (password_verify($password, $user['password'])) {
+                return $user;
+            }
         }
+    
     }
-
     return FALSE;
 }
 
 function getCountries() {
-    $pdo = load("root");
-    $stmt = $pdo->query("SELECT ID_PAIS as id, nombre  FROM PAISES");
-    $countries = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if($pdo = load("root")) {
+        $stmt = $pdo->query("SELECT ID_PAIS as id, nombre  FROM PAISES");
+        $countries = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($countries as $country) {
-        echo "<option value='$country[id]'>$country[nombre]</option>";
+        foreach ($countries as $country) {
+            echo "<option value='$country[id]'>$country[nombre]</option>";
+        }
     }
 }
 
 
 function getInstitutionTypes(){
-    $pdo = load("root");
-    $stmt = $pdo->query("SELECT ID_TIPO_INSTITUCION, TIPO FROM TIPOS_INSTITUCION;");
-    $insTypes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    foreach($insTypes as $type){
-        echo "<option value='$type[ID_TIPO_INSTITUCION]]'>$type[TIPO]</option>";
+    if($pdo = load("root")) {
+        $stmt = $pdo->query("SELECT ID_TIPO_INSTITUCION, TIPO FROM TIPOS_INSTITUCION;");
+        $insTypes = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
+        foreach($insTypes as $type){
+            echo "<option value='$type[ID_TIPO_INSTITUCION]]'>$type[TIPO]</option>";
+        }
     }
-    
 }
 
 
 
 function registerPartnerInstitution(){
+    if($pdo = load("root")) {
+        try {
+            $name = $_POST["fName"]." ".$_POST["lName"];
+            $email = $_POST["email"];
+            $phone = $_POST["phone"];
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $vat = ($_POST["vat"]) ? $_POST["vat"] : NULL;
+            $post = $_POST["post"];
+            $department = $_POST["department"];
+            $l_provider = 0; //not a provider
+            $rol = 2; // registered user
+            $country = $_POST["country"];
+
+            $params = [
+                ':name' => $name,
+                ':email' => $email,
+                ':phone' => $phone,
+                ':username' => $username,
+                ':password' => password_hash($password, PASSWORD_DEFAULT),
+                ':vat' => $vat,
+                ':post' => $post,
+                ':department' => $department,
+                ':l_provider' => $l_provider,
+                ':rol' => $rol,
+                ':country' => $country
+            ];
+
+            $pdo->beginTransaction();
+            $stmt = $pdo->prepare("INSERT INTO SOCIOS (NOMBRE_COMPLETO,EMAIL,TELEFONO,USUARIO,PASSWORD,VAT,CARGO,DEPARTAMENTO,R_ALOJAMIENTO,ROL,PAIS)"
+                    . "VALUES (:name, :email, :phone, :username, :password, :vat, :post, :department, :l_provider, :rol, :country);");
+            $stmt->execute($params);
+
+            
+            $location;
+            
+            if($pdo->commit()) {
+                $user = [
+                'usuario' => $params[':username'],
+                'email' => $params[':email'],
+                'password' => $params[':password']
+                ];
+            
+                $_SESSION['user'] = $user;
+            
+                $location = "Location:index.php?registered";
+            } else {
+                $location = "Location: register.php?error";
+            }
+
+            header($location);
+            
+            
+        } catch(Exception $e){
+            $pdo->rollBack();
+            header("Location: register.php?error");
+        }
     
-    $pdo = load("root");
-    try {
-        $name=$_POST["fName"]+$_POST["lName"];
-        $pdo->beginTransaction();
-        $stmt = $pdo->prepare("INSERT INTO PARTNER (NOMBRE_COMPLETO,EMAIL,TELEFONO,USUARIO,PASSWORD,VAT,CARGO,DEPARTAMENTO,R_ALOJAMIENTO,PAIS)"
-                . "VALUES (?,?,?,?,?,?,?,?,?,?);");
-        $stmt = $pdo->exec($name,$_POST[""],$_POST[""],$_POST[""],$_POST[""],$_POST[""],$_POST[""],$_POST[""],$_POST[""],$_POST[""]);
-        
-        
-        header("Location:index.php");
-        
-    }catch(Exception $e){
-        
     }
-    
-    
 }
