ccf3ba2c619c081fa184ba818e756765a3c016b5
unknown <jorgemarti123@hotmail.com>
2020-03-15 19:08:38 +0100

    Añadido permisos al usuario de login para acceder a la tabla ROl_USUARIOS

--- BD/scriptUsuariosBD.sql
+++ BD/scriptUsuariosBD.sql
@@ -2,6 +2,7 @@ CREATE USER IF NOT EXISTS 'movilidades_login'@'localhost' IDENTIFIED WITH mysql_
 CREATE USER IF NOT EXISTS 'movilidades_admin'@'localhost' IDENTIFIED WITH mysql_native_password USING PASSWORD('abc123.');
 CREATE USER IF NOT EXISTS 'movilidades_registered'@'localhost' IDENTIFIED WITH mysql_native_password USING PASSWORD('abc123.');
 GRANT SELECT ON GESTIONMOVILIDADES.SOCIOS TO 'movilidades_login'@'localhost';
+GRANT SELECT ON GESTIONMOVILIDADES.ROL_USUARIOS TO 'movilidades_login'@'localhost';
 GRANT SELECT ON GESTIONMOVILIDADES.PAISES TO 'movilidades_login'@'localhost';
 GRANT SELECT ON GESTIONMOVILIDADES.TIPOS_INSTITUCION TO 'movilidades_login'@'localhost';
 GRANT INSERT ON GESTIONMOVILIDADES.SOCIOS TO 'movilidades_login'@'localhost';
