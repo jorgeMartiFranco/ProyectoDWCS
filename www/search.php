82c153995003d90b5b9fa866d385ab652244d375
unknown <jorgemarti123@hotmail.com>
2020-03-18 13:27:00 +0100

    Implementada búsqueda básica funcional

--- www/search.php
+++ www/search.php
@@ -1,13 +1,35 @@
+<?php?>
+<html>
+<head>
+		<title>Mobility&sharp;</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, user-scalable=no" />
+                <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/custom.css" />
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.bundle.min.js"></script>
+		<script src="js/custom.js"></script>
+	</head>
+        <body>
 <?php
-if(isset($_GET['institutionName'])) {
+include "controller\db.php";
+if((isset($_GET['enterpriseName']) or !empty($_GET["enterpriseName"]))) {
+    include "header.php";
+    \MobilitySharp\controller\searchInstitutionsAdvanced();
+    include "footer.html";
+    ?>
 
+
+    
+   <?php 
 } else if(isset($_SESSION)){
 ?>
 <div class="row">
-    <form class="col col-md-10 col-lg-8 col-xl-6 mx-auto my-5" method="GET" action="/search">
+    <form class="col col-md-10 col-lg-8 col-xl-6 mx-auto my-5" method="GET" action="search.php">
         <div class="form-group">
             <label for="dbSearch">Search institutions</label>
-            <input class="form-control" type="text" id="dbSearch" placeholder="Institution name" name="institutionName">
+            <input class="form-control" type="text" id="dbSearch" placeholder="Institution name" name="enterpriseName">
             <?php if(is_null($user)) { ?>
                 <small id="dbSearchHelp" class="form-text text-muted">To perform more specific queries, registration is required</small>
             <?php } else { ?>
@@ -23,4 +45,7 @@ if(isset($_GET['institutionName'])) {
 } else {
     header('Location: /');
 }
+
 ?>
+        </body>
+</html>
