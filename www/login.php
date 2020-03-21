18de30321d676111c1a6c56d7a617ce1bab1f937
Diego Rodríguez Vicente <diego_pkv@hotmail.com>
2020-03-21 22:34:28 +0100

    Corregido un fallo de seguridad en el que no se redirigía a ninguna página

--- www/login.php
+++ www/login.php
@@ -1,6 +1,7 @@
 <?php
 	include_once 'controller/db.php';
 	use MobilitySharp\controller;
+	$location = '/';
 
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
@@ -10,9 +11,10 @@
 			if($user = controller\checkUser($username, $password)) {
 				session_start();
 				$_SESSION['user'] = $user;
-                                
 			}
 		}
+
+		$location = $_POST['redirect'] ?? $location; 
 	}
-	header('Location: '.$_POST['redirect']);
+	header('Location: '. $location);
 ?>
